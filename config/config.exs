f38fe218474c71f1838e2455b55d9e0789db77fb
Xunafay <me@xunafay.io>
2021-07-02 13:05:39 +0200

    build: add secrets config files

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,6 @@
 import Config
 
-import_config("#{config_env()}.exs")
+import_config "#{Mix.env()}.exs"
+if File.exists?("config/#{Mix.env()}.secrets.exs") do
+  import_config "#{Mix.env()}.secrets.exs"
+end
