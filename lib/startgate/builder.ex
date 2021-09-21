bd8107f9eee0528928fb8f32e2aedc42147f1d1c
Xunafay <me@xunafay.io>
2021-09-21 20:23:17 +0200

    chore: cleanup of code
    remove CI that did not aid in development
    rename files to new project name
    remove stargate as it's now a being developed as a dependency

--- lib/startgate/builder.ex
+++
@@ -1,7 +0,0 @@
-defmodule Stargate.Builder do
-  def bot() do
-    token = Application.fetch_env!(:boomba, :token)
-    headers = [Authorization: "Bot #{token}"]
-    HTTPoison.get!("https://discord.com/api/v9/gateway/bot", headers)
-  end
-end
